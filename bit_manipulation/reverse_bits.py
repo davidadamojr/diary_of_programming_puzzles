70aab56f2fd83c14527eaac47add5e8df129833f
David Adamo Jr. <me@davidadamojr.com>
2015-07-07 06:34:06 -0500

    added two problems: "intersect and reverse_bits"

---
+++ bit_manipulation/reverse_bits.py
@@ -0,0 +1,34 @@
+"""
+Reverse bits of a given 32-bit unsigned integer.
+
+For example, given input 43261596 (represented in binary as 
+00000010100101000001111010011100), return 964176192 (represented in binary as 
+00111001011110000010100101000000).
+"""
+
+# @param n, an integer
+# @return an integer
+def reverse_bits(n):
+    """
+    Repeatedly shift right and bitwise and each bit to 1
+    If the bit is 0, then the bitwise and operation is 0
+    If the bit is 1, then the bitwise and operation is 1
+    """
+    
+    if n == 0:
+        return 0
+    
+    current_value = n
+    reversed_bin_string = ""
+    for i in range(0, 32):
+        if current_value & 1 == 1:
+            reversed_bin_string = reversed_bin_string + "1"
+        else:
+            reversed_bin_string = reversed_bin_string + "0"
+        
+        current_value = current_value >> 1
+    
+    return int(reversed_bin_string, 2)
+
+if __name__ == '__main__':
+    print reverse_bits(43261596)
---
+++ mathematics_and_probability/intersect.py
@@ -0,0 +1,31 @@
+"""
+Given two lines on a Cartesian plane, determine whether the two lines would 
+intersect.
+"""
+
+class Line:
+    def __init__(self, slope, yIntercept):
+        self.slope = slope
+        self.yIntercept = yIntercept
+
+def intersect(line1, line2):
+    """
+    If two different lines are not parallel, then they intersect.
+    To check if two lines intersect, we just need to check if the slopes are 
+    different (or if the lines are identical)
+    
+    Note: Due to the limitations of floating point representations, never check 
+    for equality with ==. Instead, check if the difference is less than an 
+    epsilon value.
+    """
+    epsilon = 0.000001 # used for floating point comparisons
+    return abs(line1.slope - line2.slope) > epsilon \
+        or abs(line1.yIntercept - line2.yIntercept) < epsilon;
+
+if __name__ == '__main__':
+    line1 = Line(0.5, 1)
+    line2 = Line(0.5, 2)
+    print intersect(line1, line2)
+
+    
+    
