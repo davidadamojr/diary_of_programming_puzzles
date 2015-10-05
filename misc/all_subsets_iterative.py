a16b24b06caf05504a5dbc6d6a93060b04e07d08
David Adamo Jr. <me@davidadamojr.com>
2015-10-05 05:51:55 -0500

    added two problems: all_subsets_iterative and all_subsets_recursive moved solutions to appropriate categories

---
+++ misc/all_subsets_iterative.py
@@ -0,0 +1,25 @@
+"""
+Write a method to return all subsets of a set
+"""
+
+# @param original_set a list of integers
+def get_all_subsets(original_set):
+    
+    # iterative solution - O(2^n) where n is the size of the original set
+    
+    all_subsets = [[]]
+    for element in original_set:
+        subsets = []
+        for subset_element in all_subsets:
+            new_subset = []
+            new_subset.extend(subset_element)
+            new_subset.append(element)
+            subsets.append(new_subset)
+        all_subsets.extend(subsets)
+    
+    return all_subsets
+
+
+if __name__ == '__main__':
+    print get_all_subsets([1,2,3,4,5,6,7,8,9,10])
+        
---
+++ recursion_and_dynamic_programming/all_subsets_recursive.py
@@ -0,0 +1,27 @@
+"""
+Write a method to return all subsets of a set
+"""
+
+all_subsets = [[]]
+
+# @param original_set a list of integers
+def get_all_subsets(original_set, n):
+    # recursive solution - O(2^n) where n is the size of the original set
+    
+    if n >= len(original_set):
+        return
+    
+    new_subsets = []
+    for subset in all_subsets:
+        new_subset = []
+        new_subset.extend(subset)
+        new_subset.append(original_set[n])
+        new_subsets.append(new_subset)
+    all_subsets.extend(new_subsets)
+    get_all_subsets(original_set, n+1)
+
+if __name__ == '__main__':
+    get_all_subsets([1,2,3,4,5], 0)
+    print all_subsets
+    
+    
