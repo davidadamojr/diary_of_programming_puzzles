f331d8733dc307425f0836c65053f95725cd51e8
David Adamo Jr. <me@davidadamojr.com>
2015-10-25 10:05:48 -0500

    added one problem: "count_twos_bf"

---
+++ misc/count_twos_bf.py
@@ -0,0 +1,25 @@
+"""
+Write a method to count the number of 2s between 0 and n
+"""
+
+# brute force solution
+# sum up the number of twos in each digit from 0 to n
+
+def number_of_twos(number):
+    count = 0
+    for i in range(2, number+1):
+        count = count + get_number_of_twos(i)
+    
+    return count
+
+def get_number_of_twos(number):
+    count = 0
+    while number > 0:
+        if number % 10 == 2:
+            count = count + 1
+        number = number / 10
+    
+    return count
+
+if __name__ == '__main__':
+    print number_of_twos(100)
