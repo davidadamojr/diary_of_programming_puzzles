52b9a4c010749d25d49c7445fd9e0ee99f336d1f
David Adamo Jr. <me@davidadamojr.com>
2015-11-17 16:14:07 -0600

    completed rabin karp

--- arrays_and_strings/rabin_karp.py
+++ arrays_and_strings/rabin_karp.py
@@ -20,17 +20,36 @@ def rabin_karp_search(text, pattern):
 	text_length = len(text)
 	pattern_length = len(pattern)
 	pattern_hash = hash(pattern)
-	substr_hash = hash(text[0:pattern_length-1])
+	substr_hash = hash(text[0:pattern_length])
 	
-	for i in range(0, text_length-pattern_length):
+	for i in range(0, text_length-(pattern_length-1)):
 		if pattern_hash == substr_hash:
-			if pattern == text[i:pattern_length]: # in case of collisions
+			if pattern == text[i:i+pattern_length]: # in case of collisions
 				return i
+		substr_hash = rolling_hash(i+1, text, pattern, substr_hash)
+		
 	return None
 	
-def hash(substr_index, substr, full_text):
-	if substr == full_text[0:len(substring)]:
-		# calculate new hash value
-	else:
-		# calculate successive hash
-		
+def hash(unhashed_str):
+	"""This hash function uses 5 as the base for hashing"""
+	hash_value = 0
+	exponent = len(unhashed_str) - 1
+	for character in unhashed_str:
+		hash_value = hash_value + ord(character) * (5**exponent)
+		exponent = exponent - 1
+	
+	return hash_value
+
+def rolling_hash(start_index, text, pattern, previous_hash):
+	exponent = len(pattern) - 1
+	old_first = ord(text[start_index-1])
+	new_last = ord(text[start_index+exponent])
+	hash_value = (5 * (previous_hash - (old_first * 5**exponent))) + \
+	             (new_last * 5**0)
+	return hash_value
+
+if __name__ == '__main__':
+	print rabin_karp_search("abracadabra", "cad")
+	
+	
+	
