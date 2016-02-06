616ac0bbff9e59372a677bc9bdcb7b3d18c64d47
David Adamo Jr. <me@davidadamojr.com>
2016-02-06 12:53:29 -0600

    updated binary tree implementation

--- trees_and_graphs/binary_tree.py
+++ trees_and_graphs/binary_tree.py
@@ -1,16 +1,23 @@
-class BinaryTree:
+class BinaryTreeNode:
     def __init__(self, value, left=None, right=None):
+        self.value = value
         self.left = left
         self.right = right
-        self.value = value
+    
+    def __str__(self):
+        return self.value
+
+class BinaryTree:
+    def __init__(self, root):
+        self.root = root
 
     def __str__(self):
-        queue = [self]
+        queue = [self.root]
         tree_str = ""
 
-        while len(queue) != 0:
+        while queue: # while queue is not empty
             current_node = queue.pop()
-            tree_str = tree_str + str(current_node.value) + ","
+            tree_str = tree_str + str(current_node) + ","
             if current_node.left:
                 queue.insert(0, current_node.left)
 
