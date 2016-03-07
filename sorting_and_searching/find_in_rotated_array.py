5ba277fce9a64d686015d959b855907fe87f0afc
David Adamo Jr. <me@davidadamojr.com>
2016-03-06 20:21:33 -0600

    added one problem: find_in_rotated_array

---
+++ sorting_and_searching/find_in_rotated_array.py
@@ -0,0 +1,47 @@
+"""
+Given a sorted array of n integers that has been rotated an unknown number of times, 
+write code to find an element in the array. You may assume that the array was originally 
+sorted in increasing order.
+"""
+
+def find_in_rotated(key, rotated_lst, start, end):
+    """
+    fundamentally binary search...
+    Either the left or right half must be normally ordered. Find out which 
+    side is normally ordered, and then use the normally ordered half to figure 
+    out which side to search to find x.
+    """
+    
+    if not rotated_lst:
+        return None
+    
+    if end < start:
+        return None
+    
+    
+    middle_idx = (start + end) / 2
+    middle_elem = rotated_lst[middle_idx]
+    leftmost_elem = rotated_lst[0]
+    rightmost_elem = rotated_lst[-1]
+    
+    if middle_elem == key:
+        return middle_idx
+    
+    if leftmost_elem < middle_elem:
+        if key >= leftmost_elem and key < middle_elem:
+            return find_in_rotated(key, rotated_lst, start, middle_idx-1)
+        else:
+            return find_in_rotated(key, rotated_lst, middle_idx+1, len(rotated_lst)-1)
+    else: 
+        if key > middle_elem and key <= rightmost_elem:
+            return find_in_rotated(key, rotated_lst, middle_idx+1, len(rotated_lst)-1)
+        else:
+            return find_in_rotated(key, rotated_lst, start, middle_idx-1)
+    
+if __name__ == '__main__':
+    assert find_in_rotated(1, [4,5,6,1,2,3], 0, 5) == 3
+    assert find_in_rotated(5, [1,2,3,4,5,6], 0, 5) == 4
+    assert find_in_rotated(5, [6,6,6,6,6,6], 0, 5) == None
+    assert find_in_rotated(7, [6,6,6,7,7,7,7], 0, 7) == 3
+    assert find_in_rotated(6, [6,6,6,6,6,6], 0, 5) == 2
+    print "All test cases passed."
