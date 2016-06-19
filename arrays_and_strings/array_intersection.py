8726bd3113e0f21f5014d5283b6ac1b9e780c626
davidadamojr <me@davidadamojr.com>
2016-06-18 19:00:51 -0500

    added problem: intersection of two arrays

---
+++ arrays_and_strings/array_intersection.py
@@ -0,0 +1,45 @@
+"""
+Given two arrays, write a function to compute their intersection.
+
+Example:
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2]
+
+Note: 
+- Each element in the result should appear as many times as it shows in both arrays
+- The result can be in any order
+"""
+
+def find_intersection(first_num_lst, second_num_lst):
+    num_hash = {}
+    for num in first_num_lst:
+        if num in num_hash:
+            num_hash[num] = num_hash[num] + 1
+        else:
+            num_hash[num] = 1
+    
+    intersection_lst = []
+    for num in second_num_lst:
+        if num in num_hash and num_hash[num] > 0:
+            intersection_lst.append(num)
+            num_hash[num] = num_hash[num] - 1
+    
+    return intersection_lst
+
+if __name__ == '__main__':
+    nums1 = [1, 2, 2, 1]
+    nums2 = [2, 2]
+    assert find_intersection(nums1, nums2) == [2, 2]
+    
+    nums1 = []
+    nums2 = [2, 2]
+    assert find_intersection(nums1, nums2) == []
+    
+    nums1 = [2, 2]
+    nums2 = []
+    assert find_intersection(nums1, nums2) == []
+    
+    nums1 = []
+    nums2 = []
+    assert find_intersection(nums1, nums2) == []
+    
+    print "All test cases passed."
