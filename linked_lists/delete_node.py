3036772a0e55711399294db86c09cb5433c2e866
davidadamojr <me@davidadamojr.com>
2016-07-02 09:33:08 -0500

    added problem: delete node

---
+++ linked_lists/delete_node.py
@@ -0,0 +1,28 @@
+"""
+Write a function to delete a node (except the tail) in a singly linked list,
+given only access to that node.
+
+Suppose the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node 
+with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
+
+https://leetcode.com/problems/delete-node-in-a-linked-list/
+"""
+
+from linked_list import LinkedListNode
+
+# @param head LinkedListNode the head of a linked list
+# @param node LinkedListNode the linked list node to be deleted
+def delete_node(node):
+    if node:
+        node.data = node.next.data
+        node.next = node.next.next
+
+if __name__ == '__main__':
+    head = LinkedListNode(1)
+    head.next = LinkedListNode(2)
+    head.next.next = LinkedListNode(3)
+    head.next.next.next = LinkedListNode(4)
+    
+    delete_node(head.next.next)
+    assert str(head) == "1, 2, 4"
+    print "All test cases passed." 
